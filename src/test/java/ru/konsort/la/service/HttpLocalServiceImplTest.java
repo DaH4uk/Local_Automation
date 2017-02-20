e43235b0233a3988f4a3848a9c8b62bc20f07bdd
Turov Danil <turovdanil@mail.ru>
2017-02-20 17:41:26 +0500

    Реализовал ввод-вывод данных

--- src/test/java/ru/konsort/la/service/HttpLocalServiceImplTest.java
+++ src/test/java/ru/konsort/la/service/HttpLocalServiceImplTest.java
@@ -1,4 +1,4 @@
-package java.ru.konsort.la.service;
+package ru.konsort.la.service;
 
 import org.junit.jupiter.api.Test;
 import ru.konsort.la.service.HttpLocalServiceImpl;
@@ -11,22 +11,37 @@ class HttpLocalServiceImplTest {
 
     @Test
     public void send_get_karat_test() throws Exception {
-        System.out.println(httpLocalServiceImpl.sendGet("http://85.26.195.226:8888/cgi-bin/current_data.py?get_data=yes"));
+        System.out.println(httpLocalServiceImpl.sendGet("/cgi-bin/current_data.py?get_data=yes"));
     }
 
     @Test
     public void send_get_sauter_read_day_test() throws Exception {
-        System.out.println(httpLocalServiceImpl.sendGet("http://85.26.195.226:8888/cgi-bin/sauter.py?read_day_sp_rk1=yes"));
+        System.out.println(httpLocalServiceImpl.sendGet("/cgi-bin/sauter.py?read_day_sp_rk1=yes"));
     }
 
     @Test
     public void send_get_sauter_read_night_test() throws Exception {
-        System.out.println(httpLocalServiceImpl.sendGet("http://85.26.195.226:8888/cgi-bin/sauter.py?read_night_sp_rk1=yes"));
+        System.out.println(httpLocalServiceImpl.sendGet("/cgi-bin/sauter.py?read_night_sp_rk1=yes"));
     }
 
     @Test
     public void send_get_sauter_read_coil_test() throws Exception {
-        System.out.println(httpLocalServiceImpl.sendGet("http://85.26.195.226:8888/cgi-bin/sauter.py?read_coil_57=yes"));
+        System.out.println(httpLocalServiceImpl.sendGet("/cgi-bin/sauter.py?read_coil_57=yes"));
+    }
+
+    @Test
+    public void send_get_sauter_set_day_temp() throws Exception {
+        System.out.println(httpLocalServiceImpl.sendGet("/cgi-bin/sauter.py?day_setpoint_rk1=25.1"));
+    }
+
+    @Test
+    public void send_get_sauter_set_night_temp() throws Exception {
+        System.out.println(httpLocalServiceImpl.sendGet("/cgi-bin/sauter.py?night_setpoint_rk1=25.1"));
+    }
+
+    @Test
+    public void send_get_sauter_write_coil() throws Exception {
+        System.out.println(httpLocalServiceImpl.sendGet("/cgi-bin/sauter.py?write_coil_57=0"));
     }
 
 }
